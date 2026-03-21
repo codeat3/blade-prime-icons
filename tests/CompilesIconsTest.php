6d44e1cdb5677a266e6cab0d19d0a50bb324d35f
Swapnil Sarwe <swapnilsarwe@gmail.com>
2026-03-21 18:47:56 +0530

    laravel 13 compatibility

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -12,7 +12,7 @@ use Codeat3\BladePrimeicons\BladePrimeiconsServiceProvider;
 class CompilesIconsTest extends TestCase
 {
     /** @test */
-    public function it_compiles_a_single_anonymous_component()
+    public function test_it_compiles_a_single_anonymous_component()
     {
         $result = svg('prime-apple')->toHtml();
 
@@ -25,7 +25,7 @@ class CompilesIconsTest extends TestCase
     }
 
     /** @test */
-    public function it_can_add_classes_to_icons()
+    public function test_it_can_add_classes_to_icons()
     {
         $result = svg('prime-apple', 'w-6 h-6 text-gray-500')->toHtml();
         $expected = <<<'SVG'
@@ -35,7 +35,7 @@ class CompilesIconsTest extends TestCase
     }
 
     /** @test */
-    public function it_can_add_styles_to_icons()
+    public function test_it_can_add_styles_to_icons()
     {
         $result = svg('prime-apple', ['style' => 'color: #555'])->toHtml();
 
@@ -48,7 +48,7 @@ class CompilesIconsTest extends TestCase
     }
 
     /** @test */
-    public function it_can_add_default_class_from_config()
+    public function test_it_can_add_default_class_from_config()
     {
         Config::set('blade-prime-icons.class', 'awesome');
 
@@ -63,7 +63,7 @@ class CompilesIconsTest extends TestCase
     }
 
     /** @test */
-    public function it_can_merge_default_class_from_config()
+    public function test_it_can_merge_default_class_from_config()
     {
         Config::set('blade-prime-icons.class', 'awesome');
 
